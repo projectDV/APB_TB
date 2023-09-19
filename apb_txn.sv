420ee06716e61fc64558e5566ccde9cc20c49ac8
Siddhartha Gupta <siddhartha.gupta119@gmail.com>
2023-09-19 22:13:40 +0530

    Create apb_txn.sv
    This is a transaction file which would later be randomized and sent as a packet across other components for driving and monitoring purposes.
    
    NOTE:- It is the simplest one which I've created by far. Later as I progress I'll try adding constraints, signals and compare options too

---
+++ apb_txn.sv
@@ -0,0 +1,10 @@
+class apb_txn;
+	//rand bit[3:0] psel;
+	//rand bit penable,pwrite;
+	rand bit [31:0]paddr,pwdata;
+	
+	function void post_randomize();
+		$display("%h,%h",paddr,pwdata);
+	endfunction
+	
+endclass
