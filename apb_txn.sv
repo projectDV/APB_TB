42dc7d7ce02dcd840f539d0a2626b7ea2ef3c29d
Siddhartha Gupta <siddhartha.gupta119@gmail.com>
2023-10-12 11:32:55 +0530

    Update apb_txn.sv
    Added PREADY and excluded PENABLE from rand

--- apb_txn.sv
+++ apb_txn.sv
@@ -1,7 +1,8 @@
 class apb_txn;
 	rand bit[3:0] psel;
-	rand bit penable,pwrite;
+	rand bit pwrite;
 	rand bit [31:0]paddr,pwdata,prdata;
+	bit pready,penable;
 	
 	function void post_randomize();
 		$display("%h,%h",paddr,pwdata);
