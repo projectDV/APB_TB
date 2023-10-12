066cff7fd2318ddac93dcf0ad25778c09dc4c1a1
Siddhartha Gupta <siddhartha.gupta119@gmail.com>
2023-10-12 10:26:00 +0530

    Update apb_txn.sv
    Added prdata

--- apb_txn.sv
+++ apb_txn.sv
@@ -1,7 +1,7 @@
 class apb_txn;
 	//rand bit[3:0] psel;
 	//rand bit penable,pwrite;
-	rand bit [31:0]paddr,pwdata;
+	rand bit [31:0]paddr,pwdata,prdata;
 	
 	function void post_randomize();
 		$display("%h,%h",paddr,pwdata);
