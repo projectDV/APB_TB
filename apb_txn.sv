0bd8d5cf769d95f2a4f8e22efdc22d893c39cf26
Siddhartha Gupta <siddhartha.gupta119@gmail.com>
2023-10-12 11:38:02 +0530

    Update apb_txn.sv

--- apb_txn.sv
+++ apb_txn.sv
@@ -1,8 +1,9 @@
 class apb_txn;
 	rand bit[3:0] psel;
-	rand bit pwrite;
-	rand bit [31:0]paddr,pwdata,prdata;
+	rand bit pwrite,presetn;
+	rand bit [31:0]paddr,pwdata;
 	bit pready,penable;
+	bit [31:0] prdata;
 	
 	function void post_randomize();
 		$display("%h,%h",paddr,pwdata);
