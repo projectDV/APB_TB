34d3f1e245bf14af9ef8bf4784512d0ad3085ce5
Siddhartha Gupta <siddhartha.gupta119@gmail.com>
2023-10-17 23:26:37 +0530

    Create apb_pkg.sv

---
+++ apb_pkg.sv
@@ -0,0 +1,10 @@
+package apb_pkg;
+`include "apn_txn"
+`include "apb_gen"
+`include "apb_drv"
+`include "apb_mon"
+`include "apb_txn"
+`include "apb_rm"
+`include "apb_sb"
+`include "apb_env"
+endpackage
