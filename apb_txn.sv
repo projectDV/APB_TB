6a05ac528884d3e2aae7df9386267cbe5378adb0
Siddhartha Gupta <siddhartha.gupta119@gmail.com>
2023-10-17 11:52:11 +0530

    Update apb_txn.sv
    Added compare method

--- apb_txn.sv
+++ apb_txn.sv
@@ -4,9 +4,24 @@ class apb_txn;
 	rand bit [31:0]paddr,pwdata;
 	bit pready,penable;
 	bit [31:0] prdata;
+
+	static int txn_id;
 	
 	function void post_randomize();
 		$display("%h,%h",paddr,pwdata);
+		txn_id++;
 	endfunction
-	
+
+	function bit compare(apb_txn txn);
+		if(this.pready==trans.pready)begin
+			foreach(this.prdata[i])begin
+				if(this.prdata[i]!=txn.prdata[i])
+					return 0;
+				else
+					return 1;
+			end
+			else
+				return 0;
+		end
+	endfunction:compare
 endclass
